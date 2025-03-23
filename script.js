44a8501a212b1be4e51f42c1120cf38583c82475
dyshaaa <dyshakrut@gmail.com>
2025-03-23 19:57:38 +0200

    buttons are clickable

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let createBtn = document.querySelector('.todo-btn')
+    let input = document.querySelector(".todo-input")
+    let list = document.querySelector(".todo-list")
+
+    createBtn.addEventListener("click", function(event){
+        // not refresh
+        event.preventDefault()
+
+        if(input.value !== ""){
+
+            addTask(input.value)
+            // clear the input field after creating task
+            input.value = ""
+
+
+        }
+    })
+
+    function addTask(inputText){
+        // creating task container
+        let task = document.createElement('div')
+        task.classList.add('todo')
+
+        // task text for container
+        let taskText = document.createElement('li')
+        taskText.textContent = inputText
+
+        // btns for container
+        let doneBtn = document.createElement('button')
+        doneBtn.classList.add('btn', 'done')
+        doneBtn.innerHTML =  '<i class="fa-solid fa-check"></i>'
+
+        let deleteBtn = document.createElement('button')
+        deleteBtn.classList.add('btn', 'delete')
+        deleteBtn.innerHTML= '<i class="fa-solid fa-trash"></i>'
+
+
+        // adding elements to container
+        task.appendChild(taskText)
+        task.appendChild(doneBtn)
+        task.appendChild(deleteBtn)
+
+        list.appendChild(task)
+
+        doneBtn.addEventListener("click", function (){
+            taskText.classList.toggle('confirmed')
+        })
+
+        // delete btn does delete
+        deleteBtn.addEventListener("click",function (event){
+            task.remove()
+        })
+
+    }
+
 
 });
